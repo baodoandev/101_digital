e8aac8c4508ae383e82e4eb2a71817e63984c668
baodoandev <quangbao2307@gmail.com>
2026-06-12 14:22:55 +0700

    feat: atomic loyalty score increment

--- src/main/java/com/coffee/platform/customer/CustomerRepository.java
+++ src/main/java/com/coffee/platform/customer/CustomerRepository.java
@@ -1,6 +1,8 @@
 package com.coffee.platform.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +11,8 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByMobileNumber(String mobileNumber);
 
     boolean existsByMobileNumber(String mobileNumber);
+
+    @Modifying
+    @Query("UPDATE Customer c SET c.loyaltyScore = c.loyaltyScore + 1 WHERE c.id = :customerId")
+    void incrementLoyalty(Long customerId);
 }
