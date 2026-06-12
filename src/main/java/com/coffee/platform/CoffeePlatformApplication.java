7c82ff0c7679f6094d15c201a63f4eaaf0a58d3d
baodoandev <quangbao2307@gmail.com>
2026-06-12 12:19:33 +0700

    chore: spring boot 3.3 maven scaffold with dependencies

---
+++ src/main/java/com/coffee/platform/CoffeePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.coffee.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffee/platform/CoffeePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
